9034e2d8380f6d87bc9614fe302b91ae26073a96
Cameron_Goldfinger <cameron0393@gmail.com>
2020-08-14 16:34:35 -0700

    type: refactor
    Update output message for when an incorrect date is selected

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -34,7 +34,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
     day = input("Please input day (monday - sunday, or all): ").lower()
     while day not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'all']:
-        day = input('day is incorrect! Please input another name')
+        day = input('Input for day is incorrect! Please input another name')
 
     print('-'*40)
     return city, month, day
